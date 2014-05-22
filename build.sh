ec9abbd576e90f3e43f903799791074dbdee8196
Damian Jason Lapidge <greydamian@users.noreply.github.com>
2014-05-22 23:18:24 +0100

    Improved build script

--- build.sh
+++ build.sh
@@ -1,25 +1,80 @@
 #! /usr/bin/env bash
 
+# compiler command
 CC="cc";
 
+# archive command
 AR="ar";
 
-SRC="./src/wordspace.c";
-OBJ="./obj/wordspace.o";
+# source files directory
+SRCDIR="./src/";
+
+# source files
+SRC="wordspace.c";
+
+# object files directory
+OBJDIR="./obj/";
+
+# object files - leave empty to auto generate
+OBJ="";
+
+# output file
 OUT="./lib/libwordspace.a";
 
+# compiler flags
 CFLAGS="";
 
+# include path flags
 IFLAGS="-I./include/";
+
+# library path flags
 LFLAGS="";
 
+# library flags
 LIBS="";
 
-mkdir -p ./obj;
+function prepend {
+    result="";
+    for e in $1;
+    do
+        result="$result $2$e";
+    done
+    result="${result:1}"; # remove leading space
+    echo "$result";
+}
 
-$CC $CFLAGS -c -o $OBJ $SRC $IFLAGS $LFLAGS $LIBS;
+if [ "$OBJ" == "" ];
+then
+    # generate list of object files
+    for f in $SRC;
+    do
+        OBJ="$OBJ $(basename $f .c).o"; # append file name, .c replaced with .o
+    done
+    OBJ="${OBJ:1}";
+fi
 
-mkdir -p ./lib;
+# prepend directories to file names
+SRC=$(prepend "$SRC" "$SRCDIR");
+OBJ=$(prepend "$OBJ" "$OBJDIR");
 
+SRCARR=($SRC);
+OBJARR=($OBJ);
+
+if [ ${#SRCARR[@]} -ne ${#OBJARR[@]} ];
+then
+    echo "error: mismatched number of source files (${#SRCARR[@]})"\
+         "to object files (${#OBJARR[@]})" 1>&2;
+    exit 1; # exit failure
+fi
+
+# build each object file
+mkdir -p $OBJDIR;
+for i in ${!SRCARR[@]};
+do
+    $CC $CFLAGS $IFLAGS -c -o ${OBJARR[$i]} ${SRCARR[$i]};
+done
+
+# build static library archive
+mkdir -p $(dirname $OUT);
 $AR rcs $OUT $OBJ;
 
